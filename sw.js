d34bb08f1132f8a854a6209e0d7977f0bf2544e8
Rasyid-13 <abuazizarrasyid@gmail.com>
2026-08-03 22:30:37 +0700

    Refactor service worker to handle notifications
    Removed console logs and added notification click handling.

--- sw.js
+++ sw.js
@@ -1,16 +1,16 @@
 // File Service Worker (sw.js)
 self.addEventListener('install', (e) => {
-    console.log('[SYNORA] Service Worker Ter-install');
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
-    console.log('[SYNORA] Service Worker Aktif');
+    return self.clients.claim();
 });
 
-self.addEventListener('fetch', (e) => {
-    // Mode Bypass: Membiarkan aplikasi mengambil data online (Firestore) secara normal
-    e.respondWith(fetch(e.request).catch(() => {
-        return new Response("Aplikasi SYNORA sedang offline.");
-    }));
+// Menangani klik pada notifikasi agar membuka aplikasi kembali
+self.addEventListener('notificationclick', (event) => {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow('/')
+    );
 });
